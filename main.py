3d91fc3cb8945ca1da33b242040d1d464b0f3cff
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-02 21:39:26 -0500

    feat: :sparkles: Create the data schema

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ from fastapi import FastAPI
 import strawberry
 from strawberry.asgi import GraphQL
 from typing import List
+
+@strawberry.type
+class Movie:
+    title: str
+    director: str
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def movies(self) -> List[Movie]:
+        movies_data = [
+            Movie(title="The Silence of the Lambs", director="Jonathan Demme"),
+            Movie(title="Lady Snowblood", director="Toshiya Fujita"),
+            Movie(title="Pulp Fiction", director="Quentin Tarantino"),
+            Movie(title="Scarface", director="Brian De Palma"),
+            Movie(title="Fight Club", director="David Fincher"),
+        ]
+        return movies_data
